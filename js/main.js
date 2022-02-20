c1ecf25af96824141a004ab85aa60c4d153c6e89
Natalia <miss.happ2012@yandex.ru>
2022-02-20 13:51:42 +0300

    Делает код более читаемым

--- js/main.js
+++ js/main.js
@@ -1,25 +1,39 @@
 const getWholeNumber = (startNumber, endNumber) => {
+
   if (startNumber < 0 || endNumber < 0) {
     return false;
-  } if (startNumber === endNumber) {
+  }
+
+  if (startNumber === endNumber) {
     return 0;
-  } const maxWholeNumber = Math.floor(Math.max(startNumber, endNumber));
+  }
+
+  const maxWholeNumber = Math.floor(Math.max(startNumber, endNumber));
   const minWholeNumber = Math.ceil(Math.min(startNumber, endNumber));
   const randomWholeNumber = Math.floor(Math.random() * (maxWholeNumber - minWholeNumber + 1)) + minWholeNumber;
+
   return randomWholeNumber;
+
 };
 
 getWholeNumber(-25, 25);
 
 const getFractionalNumber = (startNumber, endNumber, numberOfCharacters) => {
+
   if (startNumber < 0 || endNumber < 0) {
     return false;
-  } if (startNumber === endNumber) {
+  }
+
+  if (startNumber === endNumber) {
     return 0;
-  } const maxNumber = Math.max(startNumber, endNumber);
+  }
+
+  const maxNumber = Math.max(startNumber, endNumber);
   const minNumber = Math.min(startNumber, endNumber);
   const randomNumber = Math.random() * (maxNumber - minNumber + 1) + minNumber;
+
   return randomNumber.toFixed(numberOfCharacters);
+  
 };
 
 getFractionalNumber(-5, 5, 2);
