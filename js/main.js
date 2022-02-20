79c488edd76c52d09d7f1d260488e689c429d160
Наталья <86907719+trofinata1@users.noreply.github.com>
2022-02-20 14:26:11 +0300

    Нужно больше функций (#2)
    * Создает функции getWholeNumber и getFractionalNumber
    
    * Создает функции getWholeNumber и getFractionalNumber
    
    * Переделывает функции getWholeNumber и getFractionalNumber
    
    * Делает код более читаемым
    
    * Делает код более читаемым
    
    * Поправляет названия переменных, корректирует поведение при одинаковом значении начального и конечного аргумента
    
    * Меняет название параметра с numberOfDigits на fractionalDigits
    
    Co-authored-by: Natalia <miss.happ2012@yandex.ru>

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const getRandomWholeNumber = (startNumber, endNumber) => {
+
+  if (startNumber < 0 || endNumber < 0) {
+    return false;
+  }
+
+  if (startNumber === endNumber) {
+    return startNumber;
+  }
+
+  const maxWholeNumber = Math.floor(Math.max(startNumber, endNumber));
+  const minWholeNumber = Math.ceil(Math.min(startNumber, endNumber));
+  const randomWholeNumber = Math.floor(Math.random() * (maxWholeNumber - minWholeNumber + 1)) + minWholeNumber;
+
+  return randomWholeNumber;
+
+};
+
+getRandomWholeNumber(25, 25);
+
+const getRandomFractionalNumber = (startNumber, endNumber, fractionalDigits) => {
+
+  if (startNumber < 0 || endNumber < 0) {
+    return false;
+  }
+
+  if (startNumber === endNumber) {
+    return startNumber;
+  }
+
+  const maxNumber = Math.max(startNumber, endNumber);
+  const minNumber = Math.min(startNumber, endNumber);
+  const randomNumber = Math.random() * (maxNumber - minNumber + 1) + minNumber;
+
+  return randomNumber.toFixed(fractionalDigits);
+
+};
+
+getRandomFractionalNumber(-5, 5, 2);
