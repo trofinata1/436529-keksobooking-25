1012075147ef48d36291c80f25cefc0f6cba4fc5
Natalia <miss.happ2012@yandex.ru>
2022-03-21 14:40:17 +0300

    Поправляет функцию setTextContent

--- js/set-text-content.js
+++ js/set-text-content.js
@@ -1,5 +1,5 @@
 export const setTextContent = (obj) => {
-  Object.entries(obj).forEach(([key, value]) => {
+  Object.entries(obj).forEach(([, value]) => {
     Object.values(value)[0].textContent = Object.values(value)[1];
   });
 };
